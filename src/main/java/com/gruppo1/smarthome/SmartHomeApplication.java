254949db7888269294db63b3ffc24f2397cc2fa2
antoniocolucci <antonio.colucci003@studenti.uniparthenope.it>
2021-08-08 17:24:51 +0200

    Commento

--- src/main/java/com/gruppo1/smarthome/SmartHomeApplication.java
+++ src/main/java/com/gruppo1/smarthome/SmartHomeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SmartHomeApplication {
     //Test github
+    //Commento prova Antonio Colucci
     public static void main(String[] args) {
         SpringApplication.run(SmartHomeApplication.class, args);
     }
