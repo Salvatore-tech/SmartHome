2cf0ba179e4ad5be500e86948f87d253386dbdc8
Salvatore-tech <salvatore.tech99@gmail.com>
2021-08-08 17:16:49 +0200

    Git pull

--- src/main/java/com/gruppo1/smarthome/SmartHomeApplication.java
+++ src/main/java/com/gruppo1/smarthome/SmartHomeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SmartHomeApplication {
     //Test github
+    //Test pull
     public static void main(String[] args) {
         SpringApplication.run(SmartHomeApplication.class, args);
     }
