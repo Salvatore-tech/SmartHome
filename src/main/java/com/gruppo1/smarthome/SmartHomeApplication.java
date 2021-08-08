1b1502e1b07afa43a0362d7ed9335eab29e62a00
pizzax11 <antonio.sarno001@studenti.uniparthenope.it>
2021-08-08 17:09:50 +0200

    Test github

--- src/main/java/com/gruppo1/smarthome/SmartHomeApplication.java
+++ src/main/java/com/gruppo1/smarthome/SmartHomeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartHomeApplication {
-
+    //Test github
     public static void main(String[] args) {
         SpringApplication.run(SmartHomeApplication.class, args);
     }
