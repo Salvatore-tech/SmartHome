061ed47c2501d87a4528f31ef37d74bf9f1f86fd
pizzax11 <antonio.sarno001@studenti.uniparthenope.it>
2021-08-13 01:12:05 +0200

    Update Controller Mapping (to check)

--- src/main/java/com/gruppo1/smarthome/controller/SceneController.java
+++ src/main/java/com/gruppo1/smarthome/controller/SceneController.java
@@ -9,10 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-<<<<<<< HEAD
 import java.util.Optional;
-=======
->>>>>>> origin/master
+
 
 @RestController
 @RequestMapping("/scene/")
@@ -23,17 +21,12 @@ public class SceneController {
         this.sceneService = sceneService;
     }
 
-<<<<<<< HEAD
     @GetMapping("/allScenes/")
-=======
-    @GetMapping("/all/")
->>>>>>> origin/master
     public ResponseEntity<List<Scene>> getAllScenes(){
         List<Scene> scenes = sceneService.findAllScene();
         return new ResponseEntity<>(scenes, HttpStatus.OK);
     }
 
-<<<<<<< HEAD
     @GetMapping("/sceneById/")
     public ResponseEntity<Optional<Scene>> getSceneById(Long id){
         Optional<Scene> scene = sceneService.findSceneByID(id);
@@ -52,9 +45,4 @@ public class SceneController {
         return new ResponseEntity<>(newScene, HttpStatus.OK);
     }
 
-
-=======
-    //TODO
-    //I don't know what else I have to implement, there is no controller in the sample
->>>>>>> origin/master
 }
