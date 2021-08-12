644c96aedf152d66c088b32958e62936bda1ff03
Martina Buffolino <86836259+martinabuffolino@users.noreply.github.com>
2021-08-12 17:23:16 +0200

    Add files via upload

---
+++ src/main/java/com/gruppo1/smarthome/model/Device.java
@@ -0,0 +1,47 @@
+package com.gruppo1.smarthome.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+
+@Entity
+public class Device implements Serializable {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Boolean status;
+
+    //TODO
+    @ManyToOne
+    private Room id_room;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(Boolean status){
+        this.status= status;
+    }
+}
