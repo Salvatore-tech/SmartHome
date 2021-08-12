afedbae07c9a749eca5475d09a1d0f862fd00096
Martina Buffolino <86836259+martinabuffolino@users.noreply.github.com>
2021-08-12 17:25:45 +0200

    Delete DeviceRepo.java

--- src/main/java/com/gruppo1/smarthome/model/DeviceRepo.java
+++
@@ -1,6 +0,0 @@
-package com.gruppo1.smarthome.repository;
-import com.gruppo1.smarthome.model.Device;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DeviceRepo extends CrudRepository<Device, Long> {
-}
