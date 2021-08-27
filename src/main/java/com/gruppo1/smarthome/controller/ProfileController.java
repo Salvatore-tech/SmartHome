97c722aee9e68c616ed1c70ec7d5ac9830f07b45
antoniocolucci <antonio.colucci003@studenti.uniparthenope.it>
2021-08-27 12:03:32 +0200

    Swagger annotations for Profile and Room.

--- src/main/java/com/gruppo1/smarthome/controller/ProfileController.java
+++ src/main/java/com/gruppo1/smarthome/controller/ProfileController.java
@@ -2,6 +2,7 @@ package com.gruppo1.smarthome.controller;
 
 import com.gruppo1.smarthome.model.Profile;
 import com.gruppo1.smarthome.service.ProfileService;
+import io.swagger.annotations.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import java.util.Optional;
 
 
 @RestController
+@Api(value = "Profile", description = "Rest API for Profile", tags = {"Profile"})
 @RequestMapping("/profile")
 public class ProfileController {
     private final ProfileService profileService;
@@ -20,34 +22,53 @@ public class ProfileController {
     }
 
     @GetMapping("/all")
+    @ApiOperation(value = "Profile", tags = {"Profile"})
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "return profile"),
+            @ApiResponse(code = 404, message = "Not Found - returned on resource not found")})
     public ResponseEntity<List<Profile>> getAllProfile(){
         List<Profile> profiles = profileService.findAllProfile();
         return new ResponseEntity<>(profiles, HttpStatus.OK);
     }
 
-    @GetMapping("/profileById")
-    public ResponseEntity<Optional<Profile>> getProfileById(String id){
+    @GetMapping("/find/{id}")
+    @ApiOperation(value = "Find profile", tags = {"Profile"})
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Return profile"),
+            @ApiResponse(code = 404, message = "Not Found - returned on resource not found")})
+    public ResponseEntity<Optional<Profile>> getProfileById(@ApiParam(value = "Profile ID", required = true)
+                                                                @PathVariable("id") String id){
         Optional<Profile> profile = profileService.findProfileByID(id);
         return new ResponseEntity<>(profile, HttpStatus.OK);
     }
 
     @PutMapping("/update")
+    @ApiOperation(value = "Update profile", tags = {"Profile"})
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Profile updated"),
+            @ApiResponse(code = 404, message = "Not Found - returned on resource not found"),
+            @ApiResponse(code = 400, message = "Bad Request")})
     public ResponseEntity<Profile> updateProfile(@RequestBody Profile profile){
         Profile updatedProfile = profileService.updateProfile (profile);
         return new ResponseEntity<>(updatedProfile, HttpStatus.OK);
     }
 
     @PostMapping("/add")
+    @ApiOperation(value = "Add new profile", tags = {"Profile"})
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Profile added"),
+            @ApiResponse(code = 405, message = "Method Not Allowed"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 500, message = "Internal Server Error")})
     public ResponseEntity<Profile> addProfile(@RequestBody Profile profile){
         Profile newProfile = profileService.addProfile(profile);
         return new ResponseEntity<>(newProfile, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/deleteProfile/{id}")
-    public ResponseEntity<?> deleteProfile(@PathVariable("id") String id){
+    @ApiOperation(value = "Delete profile", tags = {"Profile"})
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Profile deleted"),
+            @ApiResponse(code = 404, message = "Not Found - returned on resource not found"),
+            @ApiResponse(code = 500, message = "Internal Server Error")})
+    public ResponseEntity<?> deleteProfile(@ApiParam(value = "Profile ID", required = true)
+                                               @PathVariable("id") String id){
         profileService.deleteProfile(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
-
 }
