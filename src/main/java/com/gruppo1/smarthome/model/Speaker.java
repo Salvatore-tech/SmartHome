813c39c476ee5b3827491cb7a64105212f97cfab
Salvatore-tech <salvatore.tech99@gmail.com>
2021-10-28 21:57:28 +0200

    Revert "Add Device"
    This reverts commit 216840f79be1afa1ce61555e0219fd93e2dea8bb.

--- src/main/java/com/gruppo1/smarthome/model/Speaker.java
+++
@@ -1,8 +0,0 @@
-package com.gruppo1.smarthome.model;
-
-public class Speaker extends Device{
-    private Integer volume;
-
-    public void setVolume(Integer volume) {this.volume = volume;}
-    public Integer getVolume(){return volume;}
-}
