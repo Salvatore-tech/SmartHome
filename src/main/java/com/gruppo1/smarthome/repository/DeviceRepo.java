aa462a7f1877d35efc465e7ed18623d27856d373
antoniocolucci <antonio.colucci003@studenti.uniparthenope.it>
2021-10-31 11:36:41 +0100

    Fix add room in Device

--- src/main/java/com/gruppo1/smarthome/repository/DeviceRepo.java
+++ src/main/java/com/gruppo1/smarthome/repository/DeviceRepo.java
@@ -1,7 +1,10 @@
 package com.gruppo1.smarthome.repository;
 
 import com.gruppo1.smarthome.model.Device;
+import com.gruppo1.smarthome.model.Room;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
