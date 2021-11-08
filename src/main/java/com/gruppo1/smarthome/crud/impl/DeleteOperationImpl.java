45fd35f58802ce9c30007d685b9a2144acd4c50f
antoniocolucci <antonio.colucci003@studenti.uniparthenope.it>
2021-11-08 16:05:24 +0100

    Change "GenericRepository" to "BaseSmartHomeRepository" in "DeleteOperationImpl" and "GetByNameOperationImpl".

--- src/main/java/com/gruppo1/smarthome/crud/impl/DeleteOperationImpl.java
+++ src/main/java/com/gruppo1/smarthome/crud/impl/DeleteOperationImpl.java
@@ -1,13 +1,13 @@
 package com.gruppo1.smarthome.crud.impl;
 
+import com.gruppo1.smarthome.crud.api.BaseSmartHomeRepository;
 import com.gruppo1.smarthome.crud.api.CrudOperation;
-import com.gruppo1.smarthome.crud.api.GenericRepository;
 import com.gruppo1.smarthome.crud.beans.ApplicationContextProvider;
 
 public class DeleteOperationImpl implements CrudOperation {
     @Override
     public Integer execute(Object item, String name) {
-        GenericRepository repository = ApplicationContextProvider.getRepository(item);
+        BaseSmartHomeRepository repository = ApplicationContextProvider.getRepository(item);
         assert repository != null;
         return repository.deleteByName(name);
     }
