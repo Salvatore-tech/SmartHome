32571e6e36f30ef6f93d0b43440da7a3c4355c3d
antoniocolucci <antonio.colucci003@studenti.uniparthenope.it>
2021-11-08 15:41:55 +0100

    Remove import of java.util.Optional

--- src/main/java/com/gruppo1/smarthome/service/DeviceService.java
+++ src/main/java/com/gruppo1/smarthome/service/DeviceService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
+
 
 @Service
 @Transactional
