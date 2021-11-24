f1ab8266f8241a819033406cc92220242b5f8bc8
antoniocolucci <antonio.colucci003@studenti.uniparthenope.it>
2021-11-24 23:19:02 +0100

    Explicit Autowired on Service in Controller class.

--- src/main/java/com/gruppo1/smarthome/controller/RoomController.java
+++ src/main/java/com/gruppo1/smarthome/controller/RoomController.java
@@ -6,6 +6,7 @@ import com.gruppo1.smarthome.model.Device;
 import com.gruppo1.smarthome.model.Room;
 import com.gruppo1.smarthome.service.RoomService;
 import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,7 @@ import java.util.Objects;
 public class RoomController {
     private final RoomService roomService;
 
+    @Autowired
     public RoomController(RoomService roomService) {
         this.roomService = roomService;
     }
