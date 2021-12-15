6610869422f3afdd6a66d8f338254ab7894c0ba8
antoniocolucci <antonio.colucci003@studenti.uniparthenope.it>
2021-12-15 16:30:18 +0100

    Fix schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,48 +11,48 @@ DROP TABLE room;
 
 CREATE TABLE profile
 (
-    id       VARCHAR(50) NOT NULL,
-    name     VARCHAR(20) not NULL,
-    surname  VARCHAR(20) not NULL,
-    email    VARCHAR(20) not NULL,
-    password VARCHAR(20) not NULL,
+    id       VARCHAR(255) NOT NULL,
+    name     VARCHAR(255) not NULL,
+    surname  VARCHAR(255) not NULL,
+    email    VARCHAR(255) not NULL,
+    password VARCHAR(255) not NULL,
     CONSTRAINT pk_profile PRIMARY KEY (id)
 );
 
 CREATE TABLE room
 (
-    id   VARCHAR(50) PRIMARY KEY,
-    name VARCHAR(20) UNIQUE NOT NULL
+    id   VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE device
 (
-    id      VARCHAR(50)        NOT NULL,
-    name    VARCHAR(20) UNIQUE NOT NULL,
+    id      VARCHAR(255)        NOT NULL,
+    name    VARCHAR(255) UNIQUE NOT NULL,
     status  BOOLEAN DEFAULT FALSE,
-    type    VARCHAR(20)        NOT NULL,
-    room_id VARCHAR(50),
+    type    VARCHAR(255)        NOT NULL,
+    room_id VARCHAR(255),
     CONSTRAINT pk_device PRIMARY KEY (id),
     FOREIGN KEY (room_id) REFERENCES room (id)
 );
 
 CREATE TABLE scene
 (
-    id     VARCHAR(50),
-    name   VARCHAR(20) UNIQUE NOT NULL,
+    id     VARCHAR(255),
+    name   VARCHAR(255) UNIQUE NOT NULL,
     status BOOLEAN DEFAULT FALSE,
-    period          VARCHAR(20),
+    period          VARCHAR(255),
     CONSTRAINT pk_scene PRIMARY KEY (id)
 );
 
 CREATE TABLE condition
 (
-    id              VARCHAR(50)        NOT NULL,
-    device_id       VARCHAR(50)        NOT NULL,
-    scene_id        VARCHAR(50)        NOT NULL,
+    id              VARCHAR(255)        NOT NULL,
+    device_id       VARCHAR(255)        NOT NULL,
+    scene_id        VARCHAR(255)        NOT NULL,
     activation_date TIMESTAMP(0),
     threshold       FLOAT,
-    name            VARCHAR(50) UNIQUE NOT NULL,
+    name            VARCHAR(255) UNIQUE NOT NULL,
     CONSTRAINT pk_condition PRIMARY KEY (id),
     FOREIGN KEY (device_id) REFERENCES device (id),
     FOREIGN KEY (scene_id) REFERENCES scene (id)
@@ -60,12 +60,11 @@ CREATE TABLE condition
 
 CREATE TABLE alarm_clock
 (
-    id      VARCHAR(50)        NOT NULL,
-    name    VARCHAR(20) UNIQUE NOT NULL,
-    status  BOOLEAN DEFAULT FALSE,
-    type    VARCHAR(20)        NOT NULL,
-    time    VARCHAR(20),
-    room_id VARCHAR(50),
+    id      VARCHAR(255)        NOT NULL,
+    time    VARCHAR(255),
+    frequency    VARCHAR(255),
+    song    VARCHAR(255),
+    room_id VARCHAR(255),
     CONSTRAINT pk_device PRIMARY KEY (id),
     FOREIGN KEY (room_id) REFERENCES room (id),
     FOREIGN KEY (id) REFERENCES device(id)
@@ -73,13 +72,10 @@ CREATE TABLE alarm_clock
 
 CREATE TABLE conditioner
 (
-    id          VARCHAR(50)        NOT NULL,
-    name        VARCHAR(20) UNIQUE NOT NULL,
-    status      BOOLEAN DEFAULT FALSE,
-    type        VARCHAR(20)        NOT NULL,
+    id          VARCHAR(255)        NOT NULL,
     temperature INTEGER,
-    settings    VARCHAR(20),
-    room_id     VARCHAR(50),
+    settings    VARCHAR(255),
+    room_id     VARCHAR(255),
     CONSTRAINT pk_device PRIMARY KEY (id),
     FOREIGN KEY (room_id) REFERENCES room (id),
     FOREIGN KEY (id) REFERENCES device(id)
@@ -87,13 +83,10 @@ CREATE TABLE conditioner
 
 CREATE TABLE light_bulb
 (
-    id         VARCHAR(50)        NOT NULL,
-    name       VARCHAR(20) UNIQUE NOT NULL,
-    status     BOOLEAN DEFAULT FALSE,
-    type       VARCHAR(20)        NOT NULL,
+    id         VARCHAR(255)        NOT NULL,
     brightness INTEGER,
     colortemp  INTEGER,
-    room_id    VARCHAR(50),
+    room_id    VARCHAR(255),
     CONSTRAINT pk_device PRIMARY KEY (id),
     FOREIGN KEY (room_id) REFERENCES room (id),
     FOREIGN KEY (id) REFERENCES device(id)
@@ -101,25 +94,19 @@ CREATE TABLE light_bulb
 
 CREATE TABLE speaker
 (
-    id      VARCHAR(50)        NOT NULL,
-    name    VARCHAR(20) UNIQUE NOT NULL,
-    status  BOOLEAN DEFAULT FALSE,
-    type    VARCHAR(20)        NOT NULL,
+    id      VARCHAR(255)        NOT NULL,
     volume  INTEGER,
-    device_fk VARCHAR(50),
+    device_fk VARCHAR(255),
     CONSTRAINT pk_device PRIMARY KEY (id),
     FOREIGN KEY (device_fk) REFERENCES device(id)
 );
 
 CREATE TABLE television
 (
-    id      VARCHAR(50)        NOT NULL,
-    name    VARCHAR(20) UNIQUE NOT NULL,
-    status  BOOLEAN DEFAULT FALSE,
-    type    VARCHAR(20)        NOT NULL,
+    id      VARCHAR(255)        NOT NULL,
     volume  INTEGER,
     channel INTEGER,
-    room_id VARCHAR(50),
+    room_id VARCHAR(255),
     CONSTRAINT pk_device PRIMARY KEY (id),
     FOREIGN KEY (room_id) REFERENCES room (id),
     FOREIGN KEY (id) REFERENCES device(id)
